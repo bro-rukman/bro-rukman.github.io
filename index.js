995d2ffcecf203ca5e631225d3df664a8b84e517
bro-rukman <rukmanaendang99@gmail.com>
2020-05-03 21:02:55 +0700

    push for portfolio

---
+++ index.js
@@ -0,0 +1,19 @@
+// no function
+// var a = 2;
+// var b = 3;
+// var vlA;
+// var vlB;
+// vlA = a * a * a;
+// vlB = b * b * b;
+// var total = vlB + vlA;
+// console.log(total);
+function kubus(a, b) {
+    // var a = 2;
+    // var b = 2;
+    var total;
+    total = a + b;
+    return total;
+
+}
+// kubus(3, 5);
+console.log(kubus(3, 5));
